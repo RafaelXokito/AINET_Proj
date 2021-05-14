2915c8f5d1f4d044e339de3718f99766eee6fe0e
RafaelXokito <82586689+RafaelXokito@users.noreply.github.com>
2021-05-14 13:44:53 +0100

    Controllers-Models
    Criação de Controladores e modelos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layout');
-});
+Route::get('/', [PageController::class, 'index'])->name('home');
+
 
